f401261e09e2f5c9b8c2c234458b9722d0635249
Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>
2024-06-18 19:12:46 -0400

    refactor(core): Remove config from test
    Signed-off-by: Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>

--- src-rust/toolkit-core/src/lib.rs
+++ src-rust/toolkit-core/src/lib.rs
@@ -2,22 +2,9 @@ mod apis;
 
 #[cfg(test)]
 mod tests {
-    use confique::Config;
-
-    #[derive(Config)]
-    pub struct AppConfig {
-        pub github_username: String,
-        pub github_token: String,
-    }
-
     use crate::apis::GitHubApi;
 
     #[tokio::test]
     async fn github_verify_user() {
-        let config = AppConfig::builder()
-            .file("config/cli.dev.toml")
-            .load().unwrap();
-        let github_api = GitHubApi::new(config.github_username, config.github_token);
-        assert!(github_api.verify_user().await);
     }
 }
