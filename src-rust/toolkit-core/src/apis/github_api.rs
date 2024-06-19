8cc9215f57ab38cbd18f7c9b158002f0fe044cc2
Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>
2024-06-18 23:31:08 -0400

    feat(core): Add repo verification
    Signed-off-by: Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>

--- src-rust/toolkit-core/src/apis/github_api.rs
+++ src-rust/toolkit-core/src/apis/github_api.rs
@@ -1,4 +1,6 @@
+use std::ptr::null;
 use octocrab::Octocrab;
+use super::super::models::GitHubRepo;
 
 pub struct GitHubApi {
     username: String,
@@ -13,11 +15,37 @@ impl GitHubApi {
         }
     }
 
+    /**
+     * Get the username (login) of the currently signed-in GitHub user.
+     */
     async fn get_user(&self) -> String {
         self.octocrab.current().user().await.unwrap().login
     }
 
+    /**
+     * Verify that the username of the currently signed-in GitHub user matches the username
+     * provided to the GitHubApi.
+     */
     pub async fn verify_user(&self) -> bool {
         self.username == self.get_user().await
+    }
+
+    /**
+     * Validate that the given repository exists and is a fork of the upstream repository.
+     */
+    pub async fn validate_repo(&self, repo: GitHubRepo, upstream: GitHubRepo) -> Result<(), Err> {
+        if let Ok(repo) = self.octocrab.repos(repo.owner, repo.name).get().await {
+            if repo.fork {
+                if repo.parent.unwrap().full_name.unwrap_or_else(null) == upstream.get_full_name() {
+                    Ok(())
+                } else {
+                    Err("Repository is not a fork of the upstream repository")
+                }
+            } else {
+                Err("Repository is not a fork")
+            }
+        } else {
+            Err("Repository does not exist")
+        }
     }
 }
---
+++ src-rust/toolkit-core/src/models/github_repo.rs
@@ -0,0 +1,14 @@
+pub struct GitHubRepo {
+    pub owner: String,
+    pub name: String,
+}
+
+impl GitHubRepo {
+    pub fn new(owner: String, name: String) -> Self {
+        Self { owner, name }
+    }
+
+    pub fn get_full_name(&self) -> String {
+        format!("{}/{}", self.owner, self.name)
+    }
+}
