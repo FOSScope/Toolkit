b02aa9637013db457a68ccc890cee1a070cbe4f7
Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>
2024-07-13 17:29:16 -0400

    feat(core): Add Undefined Action Command Handling
    Signed-off-by: Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>

--- src-rust/toolkit-core/src/models/action_command.rs
+++ src-rust/toolkit-core/src/models/action_command.rs
@@ -30,24 +30,27 @@ impl ActionCommand {
         }
     }
 
-    pub fn execute(&self) {
+    pub fn execute(&self) -> Result<(), &str> {
         match self.command.as_str() {
             // Copy a file or a directory
             "CP" => {
                 let src = self.args.get(0).unwrap();
                 let dest = self.args.get(1).unwrap();
                 std::fs::copy(src, dest).unwrap();
+                Ok(())
             }
             // Create a directory
             "MKDIR" => {
                 let path = self.args.get(0).unwrap();
                 std::fs::create_dir_all(path).unwrap();
+                Ok(())
             }
             // Move a file or a directory
             "MV" => {
                 let src = self.args.get(0).unwrap();
                 let dest = self.args.get(1).unwrap();
                 std::fs::rename(src, dest).unwrap();
+                Ok(())
             }
             // Remove a file or a directory
             "RM" => {
@@ -57,13 +60,15 @@ impl ActionCommand {
                 } else {
                     std::fs::remove_file(path).unwrap();
                 }
+                Ok(())
             }
             // Create a file
             "TOUCH" => {
                 let path = self.args.get(0).unwrap();
                 std::fs::File::create(path).unwrap();
+                Ok(())
             }
-            _ => {}
+            _ => Err("Unknown command"),
         }
     }
 }
