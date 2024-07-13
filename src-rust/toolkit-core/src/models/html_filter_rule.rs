db518aa14e5fc85939d4e3310a3b3dea4c6210ba
Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>
2024-07-12 20:37:02 -0400

    refactor: Hardcode HTML Filter Rule in Code
    Signed-off-by: Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>

--- src-rust/toolkit-core/src/models/html_filter_rule.rs
+++ src-rust/toolkit-core/src/models/html_filter_rule.rs
@@ -3,11 +3,32 @@ pub struct HTMLFilterRule<'a> {
     pub classes: Vec<&'a str>,
 }
 
-impl HTMLFilterRule {
-    pub fn new(tags: Vec<&str>, classes: Vec<&str>) -> Self {
+impl<'a> HTMLFilterRule<'a> {
+    fn new(tags: Vec<&'a str>, classes: Vec<&'a str>) -> Self {
         Self {
             tags,
             classes,
+        }
+    }
+
+    pub fn get_filter_rule(url: &str) -> Self {
+        match url {
+            "itsfoss.com" | "news.itsfoss.com" => {
+                Self::new(
+                    vec![
+                        "script", "style", "link", "meta", "li", "desc", "title", "svg", "path",
+                        "dialog", "select", "head", "header", "foot", "footer", "ul", "nav", "button",
+                        "form", "input", "picture", "time", "h2", "h3", "h4", "i", "aside",
+                        "FreeStarVideoAdContainer", "freestar-video-parent", "reestar-video-child",
+                    ],
+                    vec![
+                        "progress-bar", "js-menu", "social-share", "post-info__readtime",
+                        "cta__description", "cta__inner", "cta__content", "hide-mobile", "js-toc",
+                        "author-card", "related-posts",
+                    ],
+                )
+            }
+            _ => Self::new(vec![], vec![])
         }
     }
 }
