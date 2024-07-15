027c33ff434934295ca38950501c062af9bc592a
Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>
2024-07-15 15:06:14 -0400

    feat(core): Add GitHub API to get file content
    Signed-off-by: Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>

--- src-rust/toolkit-core/src/apis/github_api.rs
+++ src-rust/toolkit-core/src/apis/github_api.rs
@@ -167,4 +167,36 @@ impl GitHubApi {
             Err(_) => Err("Failed to parse fork response"),
         }
     }
+
+    /// Get the **decoded** content of a file in a repository.
+    ///
+    /// # Arguments
+    /// - `repo`: The repository to get the file from.
+    /// - `path`: The path to the file in the repository.
+    ///
+    /// # Returns
+    /// - `Result<String, &str>`
+    ///     - `Ok(String)`: The decoded content of the file.
+    ///     - `Err(&str)`: An error message indicating why the method failed.
+    pub async fn get_file_content(&self, repo: GitHubRepo, path: &str) -> Result<String, &str> {
+        let repo = self.octocrab.repos(repo.owner, repo.name);
+        let content = repo
+            .get_content()
+            .path(path)
+            .r#ref("main")
+            .send()
+            .await;
+        match content {
+            Ok(mut content) => {
+                let contents = content.take_items();
+                let c = &contents[0];
+                let decoded_content = c.decoded_content();
+                match decoded_content {
+                    Some(decoded_content) => Ok(decoded_content),
+                    None => Err("No file content found"),
+                }
+            },
+            Err(_) => Err("Failed to get file content"),
+        }
+    }
 }
---
+++ src-rust/toolkit-core/tests/github_file_getter_test.rs
@@ -0,0 +1,70 @@
+use octocrab::Octocrab;
+use serde::{Deserialize, Serialize};
+use wiremock::{
+    matchers::{method, path},
+    Mock, MockServer, ResponseTemplate,
+};
+
+use mock_error::setup_error_handler;
+
+mod mock_error;
+
+// This is a fake page that we can use to deserialize the response from the GitHub API.
+#[derive(Serialize, Deserialize)]
+struct FakePage<T> {
+    items: Vec<T>,
+}
+
+// This function sets up the mock server with the given template (response).
+async fn setup_api(template: ResponseTemplate) -> MockServer {
+    let mock_server = MockServer::start().await;
+
+    let mocked_path = "/repos/octocat/Hello-World/contents/README";
+
+    Mock::given(method("GET"))
+        .and(path(mocked_path))
+        .respond_with(template)
+        .mount(&mock_server)
+        .await;
+    setup_error_handler(
+        &mock_server,
+        &format!("GET on {mocked_path} was not received"),
+    ).await;
+
+    mock_server
+}
+
+// This function sets up the Octocrab client with the base URI of the mock server.
+fn setup_octocrab(uri: &str) -> Octocrab {
+    Octocrab::builder().base_uri(uri).unwrap().build().unwrap()
+}
+
+#[cfg(test)]
+mod tests {
+    use octocrab::models::repos::Content;
+
+    use fosscopetoolkit_core::apis::github_api::GitHubApi;
+    use fosscopetoolkit_core::models::github_repo::GitHubRepo;
+
+    use super::*;
+
+    #[tokio::test]
+    async fn get_file() {
+        let mocked_response: Content =
+            serde_json::from_str(include_str!("resources/get_file.json")).unwrap();
+        let template = ResponseTemplate::new(200).set_body_json(&mocked_response);
+        let mock_server = setup_api(template).await;
+        let client = setup_octocrab(&mock_server.uri());
+
+        let github = GitHubApi::new("octocat".to_string(), client);
+        let file_content = github.get_file_content(
+            GitHubRepo {
+                owner: "octocat".to_string(),
+                name: "Hello-World".to_string(),
+            },
+            "README",
+        ).await;
+        assert!(file_content.is_ok());
+        assert_eq!(file_content.unwrap().trim(), "Hello World!");
+    }
+}
