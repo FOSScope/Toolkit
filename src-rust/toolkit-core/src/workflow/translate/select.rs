e2622530b0a58601c37db50db8540f13f9a74c64
Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>
2024-07-15 15:43:47 -0400

    refactor(core): Remove old article template appender
    Signed-off-by: Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>

--- src-rust/toolkit-core/src/workflow/translate/select.rs
+++ src-rust/toolkit-core/src/workflow/translate/select.rs
@@ -1,17 +1,11 @@
 use std::collections::HashMap;
+
 use handlebars::Handlebars;
+
 use crate::models::html_filter_rule::HTMLFilterRule;
 use crate::models::repo_rule::{Article, RepoRule};
 
 pub fn get_content(url: &str) -> Result<String, String> {
-    let article_template = std::fs::read_to_string("rsc/translate_article_template.md");
-    let article_template = match article_template {
-        Ok(article_template) => article_template,
-        Err(_) => {
-            return Err("Failed to read the translate article template.".to_string());
-        }
-    };
-
     let website = url::Url::parse(url);
     let website = match website {
         Ok(website) => website,
@@ -35,15 +29,7 @@ pub fn get_content(url: &str) -> Result<String, String> {
         &*html_filter_rule.classes
     );
 
-    // Parse HTML to markdown
-    let md = html2md::parse_html(&filtered_html).trim().to_string();
-
-    // Add header and footer
-    let mut content = String::new();
-    content.push_str(&article_template);
-    content.push_str(&md);
-
-    Ok(content)
+    Ok(html2md::parse_html(&filtered_html).trim().to_string())
 }
 
 pub fn select_article(
