0e69da2f9eabc54d3ce39e38bc355f578994ead1
Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>
2024-09-23 22:33:41 -0400

    feat: Add method to decode file content
    Signed-off-by: Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>

--- src-rust/toolkit-core/src/models/github_api_responses/repository_content.rs
+++ src-rust/toolkit-core/src/models/github_api_responses/repository_content.rs
@@ -24,6 +24,29 @@ pub struct FileContent {
     pub links: Links,
 }
 
+impl FileContent {
+    /// Get the decoded content of the GitHub file content.
+    ///
+    /// If the encoding is base64, the content will be decoded and returned.
+    /// If the encoding is not base64, `None` will be returned.
+    ///
+    /// Decode Code Credit: https://github.com/XAMPPRocky/octocrab/blob/bd8b648282f4a912c839dc0f73f72c5b8d891d57/src/models/repos.rs#L228, licensed under the MIT license.
+    pub fn decoded_content(&self) -> Option<String> {
+        match self.encoding.as_deref() {
+            Some("base64") => {
+                use base64::Engine;
+                self.content.as_ref().map(|c| {
+                    let mut content = c.as_bytes().to_owned();
+                    content.retain(|b| !b" \n\t\r\x0b\x0c".contains(b));
+                    let c = base64::prelude::BASE64_STANDARD.decode(content).unwrap();
+                    String::from_utf8_lossy(&c).into_owned()
+                })
+            }
+            _ => None
+        }
+    }
+}
+
 #[derive(Serialize, Deserialize)]
 pub struct DirContent {
     pub size: usize,
